b8b709c03265fa37ffde1e1fff0c18bb50b1c960
Jan Rodan <jan.rodan@prodyna.com>
2016-09-21 12:22:26 +0200

    modified ext cleanup and showing of log

--- src/ext-clean.sh
+++ src/ext-clean.sh
@@ -3,7 +3,9 @@ JR_SCRIPTS_BASE=$1
 JR_PROJECT_HOME_NAME=$2
 . $JR_SCRIPTS_BASE/env/$JR_PROJECT_HOME_NAME.sh "$JR_SCRIPTS_BASE"
 
-exec "$JR_SCRIPTS_SRC/clean-tomcat.sh" start "$@"
+#exec "$JR_SCRIPTS_SRC/clean-tomcat.sh" start "$@"
+
+#echo "$JR_TOMCAT_ROOT"
 
 find "$JR_TOMCAT_ROOT/WEB-INF/lib/" -name "$JR_EXT*.jar" -exec rm -rf {} \;
 find "$JR_TOMCAT_ROOT/WEB-INF/" -name "$JR_EXT*.xml" -exec rm -rf {} \;
--- src/less.sh
+++ src/less.sh
@@ -4,13 +4,16 @@ JR_PROJECT_HOME_NAME=$2
 . $JR_SCRIPTS_BASE/env/$JR_PROJECT_HOME_NAME.sh "$JR_SCRIPTS_BASE"
 
 osascript <<END
-tell application "iTerm"
- tell the first terminal
-  launch session "Default Session"
-  tell the last session
-   write text "less $JR_TOMCAT/logs/catalina.out"
-   tell application "System Events" to keystroke "f" using {shift down}
-  end tell
- end tell
+tell application "iTerm2"
+	tell current window
+        create tab with default profile
+        tell the last tab
+        	select
+    	end tell  
+    	tell current session
+    		write text "less $JR_TOMCAT/logs/catalina.out"
+    		tell application "System Events" to keystroke "f" using {shift down}
+    	end tell  
+    end tell
 end tell
 END
